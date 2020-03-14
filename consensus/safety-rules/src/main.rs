968974203051dcf00d088e78d669e508d9d4bed3
Ankush Agarwal <ankushagarwal11@gmail.com>
2020-03-13 18:02:11 -0700

    main

--- consensus/safety-rules/src/main.rs
+++ consensus/safety-rules/src/main.rs
@@ -6,28 +6,33 @@
 #![forbid(unsafe_code)]
 
 use libra_config::config::NodeConfig;
-use safety_rules::Process;
-use std::{env, process};
+use safety_rules::{simple_push_metrics::Metrics, Process};
+use std::{env, process, sync::Arc};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 2 {
-        eprintln!("Incorrect number of parameters, expected a path to a config file");
-        process::exit(1);
-    }
-
-    let config = NodeConfig::load(&args[1]).unwrap_or_else(|e| {
-        eprintln!("Unable to read provided config: {}", e);
-        process::exit(1);
-    });
-
-    libra_logger::Logger::new()
-        .channel_size(config.logger.chan_size)
-        .is_async(config.logger.is_async)
-        .level(config.logger.level)
-        .init();
-
-    let mut service = Process::new(config);
-    service.start();
+    let cp = safety_rules::simple_push_metrics::CountersPusher {};
+    let metrics: Arc<dyn Metrics + Send + Sync> =
+        Arc::new(safety_rules::counters::SafetyRulesMetrics::new());
+    let handle = cp.start(Arc::clone(&metrics));
+    handle.join().unwrap();
+    // let args: Vec<String> = env::args().collect();
+    //
+    // if args.len() != 2 {
+    //     eprintln!("Incorrect number of parameters, expected a path to a config file");
+    //     process::exit(1);
+    // }
+    //
+    // let config = NodeConfig::load(&args[1]).unwrap_or_else(|e| {
+    //     eprintln!("Unable to read provided config: {}", e);
+    //     process::exit(1);
+    // });
+    //
+    // libra_logger::Logger::new()
+    //     .channel_size(config.logger.chan_size)
+    //     .is_async(config.logger.is_async)
+    //     .level(config.logger.level)
+    //     .init();
+    //
+    // let mut service = Process::new(config);
+    // service.start();
 }
